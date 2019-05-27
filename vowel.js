cd92bffaf5b137f14f1c7d9102a57f598abec6b3
Hiroshi Suda <suda@net.it-chiba.ac.jp>
2019-05-27 14:28:04 +0900

    母音の図を挿入

--- vowel.js
+++ vowel.js
@@ -6,14 +6,17 @@ class Vowel {
     console.log(this.rect);
     this.audioCtx = new AudioContext();
 
+    //    this.id.addEventListener('mousemove', (e) => {
 
-//    this.id.addEventListener('mousemove', (e) => {
-
-//    })
+    //    })
   }
 }
 
 window.addEventListener("load", () => {
+  //document.querySelector('#base').addEventListener('mousemove',()=>{return false;},true);
+  //document.querySelector('#base').addEventListener('mousedown',()=>{return false;},true);
+  //document.querySelector('#base').addEventListener('mouseup',()=>{return false;},true);
+  //document.querySelector('#base').addEventListener('click',()=>{return false;},true);
   var vowel = null;
   document.querySelector("#start").addEventListener("click", () => {
     vowel = new Vowel(document.querySelector("#can"));
@@ -27,60 +30,60 @@ window.addEventListener("load", () => {
 
   function can_init() {
     can = document.getElementById("can");
+    base = document.getElementById('base');
     ctx = can.getContext("2d");
-    can.addEventListener("mousemove", function(e) {
-      
+    base.addEventListener("mousemove", function(e) {
       if (mouse_condition) {
         var rect = e.target.getBoundingClientRect();
         var mex = e.clientX - rect.left;
         var mey = e.clientY - rect.top;
 
         let freq1 = mex * 2.0 + 200.0;
-        let freq2 = ( 640 - mey ) * 6.0 + 600.0;
+        let freq2 = (640 - mey) * 6.0 + 600.0;
         vowel.osc1.frequency.value = freq1;
         vowel.osc2.frequency.value = freq2;
-        console.log( freq2 );
+        console.log(freq2);
         ctx.beginPath();
         if (drag_color < 255) drag_color += 1;
         ctx.fillStyle =
           "rgb(" + drag_color + "," + drag_color + drag_color + ")";
         ctx.arc(mex, mey, 5, 0, Math.PI * 2, false);
         ctx.fill();
-        data.push([mex, mey]);
+        //data.push([mex, mey]);
       }
     });
-    can.addEventListener("mousedown", function(e) {
-        vowel.osc1 = vowel.audioCtx.createOscillator();
-        vowel.osc2 = vowel.audioCtx.createOscillator();
-        let amp = vowel.audioCtx.createGain();
-        amp.gain.value = 0.5;
-        vowel.osc1.type = "sine";
-        vowel.osc2.type = "sine";
-        vowel.osc1.connect(vowel.audioCtx.destination);
-        //vowel.osc2.connect(vowel.audioCtx.destination);
-        vowel.osc2.connect(amp);
-        amp.connect(vowel.audioCtx.destination);
-        var rect = e.target.getBoundingClientRect();
-        var mex = e.clientX - rect.left;
-        var mey = e.clientY - rect.top;
+    base.addEventListener("mousedown", function(e) {
+      vowel.osc1 = vowel.audioCtx.createOscillator();
+      vowel.osc2 = vowel.audioCtx.createOscillator();
+      let amp = vowel.audioCtx.createGain();
+      amp.gain.value = 0.5;
+      vowel.osc1.type = "sine";
+      vowel.osc2.type = "sine";
+      vowel.osc1.connect(vowel.audioCtx.destination);
+      //vowel.osc2.connect(vowel.audioCtx.destination);
+      vowel.osc2.connect(amp);
+      amp.connect(vowel.audioCtx.destination);
+      var rect = e.target.getBoundingClientRect();
+      var mex = e.clientX - rect.left;
+      var mey = e.clientY - rect.top;
 
-        let freq1 = mex * 2.0 + 200.0;
-        let freq2 = ( 640 - mey ) * 6.0 + 600.0;
-        vowel.osc1.frequency.value = freq1;
-        vowel.osc2.frequency.value = freq2;
-console.log( freq2 );
+      let freq1 = mex * 2.0 + 200.0;
+      let freq2 = (640 - mey) * 6.0 + 600.0;
+      vowel.osc1.frequency.value = freq1;
+      vowel.osc2.frequency.value = freq2;
+      console.log(freq2);
 
-        vowel.osc1.start();
-        vowel.osc2.start();
+      vowel.osc1.start();
+      vowel.osc2.start();
       mouse_condition = true;
       drag_color = 0;
       data.length = 0;
     });
-    can.addEventListener("mouseup", function(e) {
-        vowel.osc1.stop();
-        vowel.osc2.stop();
+    base.addEventListener("mouseup", function(e) {
+      vowel.osc1.stop();
+      vowel.osc2.stop();
       mouse_condition = false;
-      console.log(">>>" + JSON.stringify(data)); // 後々，通信部分を追加する予定
+      //console.log(">>>" + JSON.stringify(data)); // 後々，通信部分を追加する予定
       // (あくまで予定)
       data.length = 0;
     });
